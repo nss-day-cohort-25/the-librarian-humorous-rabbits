f1004e782d82ee37ee643a51cab1fd900991340f
Ryoung27 <RRYoung89@gmail.com>
2018-04-20 15:14:12 -0500

    added databases

---
+++ books.js
@@ -0,0 +1,81 @@
+const books = Object.create({}, {
+    cryptonomicon: {
+        enumerable: false,
+        writable: true,
+        title: "Cryptonomicon",
+        genre: "Fiction",
+        isbn: "0-380-97346-4",
+        checkOut: false,
+        dateDue: ""
+    },/*
+    harryPotter: {
+        enumerable: false,
+        writable: true,
+        title: "Harry Potter",
+        author: "J.K Rowling",
+        genre: "Fantasy",
+        isbn: "9788700631625",
+        checkedOut: false,
+        dateDue: ""
+    },
+    theAlchemist: {
+        enumerable: false,
+        writable: true,
+        title: "The Alchemist",
+        author: "Paulo Coelho",
+        genre: "Fiction",
+        isbn: "9789029509657",
+        checkedOut: false,
+        dateDue: ""
+    },
+    eastOfEden: {
+        enumerable: false,
+        writable: true,
+        title: "East of Eden",
+        author: "John Steinbeck",
+        genre: "Novel",
+        isbn: "9788253020419",
+        checkedOut: false,
+        dateDue: ""
+    },
+    infiniteJest: {
+        enumerable: false,
+        writable: true,
+        title: "Infinite Jest",
+        author: "David Foster Wallace",
+        genre: "Satire",
+        isbn: "9783763262847",
+        checkedOut: false,
+        dateDue: ""
+    },
+    battleRoyale: {
+        enumerable: false,
+        writable: true,
+        title: "Battle Royale",
+        author: "Koushun Takami",
+        genre: "Horror",
+        isbn: "9781470346133",
+        checkedOut: false,
+        dateDue: ""
+    },
+    theGodFather: {
+        enumerable: false,
+        writable: true,
+        title: "The Godfather",
+        author: "Mario Puzo",
+        genre:"Crime",
+        isbn: "9785040040636",
+        checkedOut: false,
+        dateDue: ""
+    },
+    noCountryForOldMen: {
+        enumerable: false,
+        writable: true,
+        title: "No Country for Old Men",
+        author: "Cormac McCarthy",
+        genre:"Thriller",
+        isbn: "9788806233983",
+        checkedOut: false,
+        dateDue: ""
+    }*/
+})
